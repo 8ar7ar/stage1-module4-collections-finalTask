1e43a8f2aea2ed56b3138b345e0bbe2b194a7b10
8ar7ar <5ar7ar@mail.ru>
2023-11-24 19:14:28 +0500

    stage1-module4-collections-finalTask "--1st attempt--locally tested--"

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> developersProjects = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet())
+            if (entry.getValue().contains(developer)) developersProjects.add(entry.getKey());
+
+        developersProjects.sort(new ProjectComparator());
+
+        return developersProjects;
+    }
+
+    public static class ProjectComparator  implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+           int result =  -(o1.length() - o2.length());
+           if (result == 0) return -o1.compareTo(o2);
+           return result;
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> result = new HashMap<>();
+        Set<String> valuesOfStrSet = new HashSet<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()){
+            result.put(entry.getKey().length(), null);
+            valuesOfStrSet.add(entry.getKey());
+        }
+
+        for (Map.Entry<Integer, Set<String>> entry : result.entrySet()){
+            Set<String> setForMap = new HashSet<>();
+            for (String val: valuesOfStrSet)
+                if (val.length()==entry.getKey())
+                    setForMap.add(val);
+            result.put(entry.getKey(), setForMap);
+        }
+        return result;
     }
 }
